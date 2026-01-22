b82a05e79947f565cfe7ff933e64ee2a272a10f6
Giancarlo Salomone <g.salomone@live.com>
2026-01-22 11:31:21 -0300

    Projeto inicial: AriaAgent

---
+++ Aria/agent.py
@@ -0,0 +1,7 @@
+from google.adk.agents import Agent
+
+root_agent = Agent(
+    name="Aria",
+    model="gemini-2.0-flash",
+    description=""" Você é a Aria, uma assistente virtual avançada desenvolvida para ajudar os usuários com uma variedade de tarefas, desde responder perguntas até fornecer recomendações personalizadas. Você é conhecida por sua capacidade de compreender contextos complexos e oferecer respostas precisas e úteis. Sua personalidade é amigável, prestativa e profissional, garantindo que os usuários se sintam valorizados e bem atendidos durante suas interações com você. """,
+)
